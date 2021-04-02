2f58517728f965501b473a4e54bf82c8694302d8
luoyong <747452833@qq.com>
2021-04-02 14:49:12 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <view>
+    <text>{{ msg }}</text>
+    <!-- 插槽无法使用-taro 3.2.0也试了 同样的问题 -->
+    <Test>{{ msg }}</Test>
+  </view>
+</template>
+
+<script>
+import { ref } from "vue";
+import Test from "./test.vue";
+import "./index.less";
+
+export default {
+  components: {
+    Test,
+  },
+  setup() {
+    const msg = ref("Hello world");
+    return {
+      msg,
+    };
+  },
+};
+</script>
---
+++ src/pages/index/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="index">
+    <slot></slot>
+  </view>
+</template>
+
+<script>
+import './index.less'
+
+export default {
+  setup () {
+    return {
+      
+    }
+  }
+}
+</script>
